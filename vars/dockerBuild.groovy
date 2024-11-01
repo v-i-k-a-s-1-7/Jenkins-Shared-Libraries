f0569ccb9659e8aa356bd50c98ea6702031226a1
Vikas Rajak <77117633+v-i-k-a-s-1-7@users.noreply.github.com>
2024-11-02 00:53:31 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
-def call(String DockerHubUser, String ProjectName, String ImageTag){
-    echo "this is Build"
-    sh "docker build -t "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" ."
+def call(String DockerHubUser, String ProjectName, String ImageTag) {
+    echo "This is Build"
+    sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
